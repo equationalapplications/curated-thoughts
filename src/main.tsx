324f4251adc67d39ff661f526c81cf9cdaf6ded3
equationalapplications <info@equationalapplications.com>
2026-05-06 12:03:33 -0400

    feat: integrate @equationalapplications/react-llm-wiki v3 with Tauri SQLite adapter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { WikiProvider } from "@equationalapplications/react-llm-wiki";
 import { App } from "./App";
+import { wiki, setupWiki } from "./lib/wiki";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+setupWiki().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <WikiProvider wiki={wiki}>
+        <App />
+      </WikiProvider>
+    </React.StrictMode>
+  );
+}).catch((err) => {
+  console.error("[wiki] setup failed, rendering without wiki:", err);
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
